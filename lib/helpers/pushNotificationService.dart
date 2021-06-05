6090133790e753fb90c434fc4f82060f350e732e
Usama137 <usama85969@gmail.com>
2021-06-06 01:44:56 +0500

    reformat

--- lib/helpers/pushNotificationService.dart
+++ lib/helpers/pushNotificationService.dart
@@ -1,13 +1,9 @@
-
 import 'package:driverapp/components/globalVariables.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class PushNotificationService {
-  //FirebaseMessaging _firebaseMessaging;
-
-  final FirebaseMessaging _firebaseMessaging=FirebaseMessaging();
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
 
   Future initialize() async {
     _firebaseMessaging.configure(
@@ -19,21 +15,20 @@ class PushNotificationService {
       },
       onResume: (Map<String, dynamic> message) async {
         print("onResume: $message");
-    },
-   );
+      },
+    );
   }
-  Future<String> getToken()async{
-    String token=  await _firebaseMessaging.getToken();
+
+  Future<String> getToken() async {
+    String token = await _firebaseMessaging.getToken();
     print('token $token');
 
-    DatabaseReference tokenRef=FirebaseDatabase.instance.reference().child('drivers/${currentFirebaseUser.uid}/token');
+    DatabaseReference tokenRef = FirebaseDatabase.instance
+        .reference()
+        .child('drivers/${currentFirebaseUser.uid}/token');
     tokenRef.set(token);
-    
+
     _firebaseMessaging.subscribeToTopic('alldrivers');
     _firebaseMessaging.subscribeToTopic('allusers');
   }
-  
 }
-
-
-
